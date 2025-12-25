a7ddcd278345f35d5b56b281420183250271d3dc
François Duthy <francois.duthy@gmail.com>
2025-12-25 21:32:21 +0000

    Add src/lib/supabase.js for frontend build

---
+++ src/lib/supabase.js
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+
+// Client Supabase pour le FRONT (ANON KEY)
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
